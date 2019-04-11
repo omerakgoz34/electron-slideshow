c95f0cc75612a2ee50452ab6025a3e0d87697e2a
omerakgoz34 <omerakgoz34@gmail.com>
2019-04-11 20:11:01 +0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function startSlideShow() {
-	setTimeout(() => {
-		document.location.assign("main.html");
-	}, 500);
+function gotoLink(x) {
+	if (x == 'slideshow') {
+		setTimeout(() => {
+			document.location.assign("main.html");
+		}, 500);
+	}
+	if (x == 'mainwebsite') {
+		setTimeout(() => {
+			document.location.assign("https://omerakgoz34.github.io");
+		}, 500);
+	}
 }
