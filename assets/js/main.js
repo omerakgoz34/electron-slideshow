90d3a5c8105999ba74294aea483af2f4e93b8889
omerakgoz34 <omerakgoz34@gmail.com>
2019-04-27 23:51:08 +0300

    New Feature
    Add: Page Control

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,37 @@
 var page = 1;
-var lastPage = 10;
+const lastPage = 5;
+
+document.getElementById("page-frame").src = "assets/pages/page" + page + ".html";
+document.getElementById("pager").innerHTML = "P:" + page;
 
 function changePage(x) {
 	if (x == "right") {
-		if (page < 10) {
+		if (page < lastPage) {
 			document.getElementById("page-frame").src = "assets/pages/page" + (page + 1) + ".html";
 			page = page + 1;
+			document.getElementById("pager").innerHTML = "P:" + page;
 		} else
-		if (page == 10 || page > 10) {
+		if (page == lastPage || page > lastPage) {
 			document.getElementById("page-frame").src = "assets/pages/page1.html";
 			page = 1;
+			document.getElementById("pager").innerHTML = "P:" + page;
 		}
 	}
 	if (x == "left") {
 		if (page > 1) {
 			document.getElementById("page-frame").src = "assets/pages/page" + (page - 1) + ".html";
 			page = page - 1;
+			document.getElementById("pager").innerHTML = "P:" + page;
 		} else
 		if (page == 1 || page < 1) {
 			document.getElementById("page-frame").src = "assets/pages/page" + lastPage + ".html";
 			page = lastPage;
+			document.getElementById("pager").innerHTML = "P:" + page;
+		}
+		if (page > lastPage) {
+			document.getElementById("page-frame").src = "assets/pages/page" + lastPage + ".html";
+			page = lastPage;
+			document.getElementById("pager").innerHTML = "P:" + page;
 		}
 	}
 }
