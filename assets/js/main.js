5dd2b7ee1e3699152b08bf06ec536c0ce0338147
omerakgoz34 <omerakgoz34@gmail.com>
2019-05-11 19:37:41 +0300

    Complate Edebiyat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,47 @@
 var page = 1;
-const lastPage = 5;
+const lastPage = 15;
 
 document.getElementById("page-frame").src = "assets/pages/page" + page + ".html";
-document.getElementById("pager").innerHTML = "P:" + page;
+document.getElementById("pager").innerHTML = page + "/" + lastPage;
 
 function changePage(x) {
 	if (x == "right") {
+		if (page == 15) alert("Slayt bitti.");
 		if (page < lastPage) {
 			document.getElementById("page-frame").src = "assets/pages/page" + (page + 1) + ".html";
 			page = page + 1;
-			document.getElementById("pager").innerHTML = "P:" + page;
+			document.getElementById("pager").innerHTML = page + "/" + lastPage;
 		} else
 		if (page == lastPage || page > lastPage) {
 			document.getElementById("page-frame").src = "assets/pages/page1.html";
 			page = 1;
-			document.getElementById("pager").innerHTML = "P:" + page;
+			document.getElementById("pager").innerHTML = page + "/" + lastPage;
 		}
 	}
 	if (x == "left") {
 		if (page > 1) {
 			document.getElementById("page-frame").src = "assets/pages/page" + (page - 1) + ".html";
 			page = page - 1;
-			document.getElementById("pager").innerHTML = "P:" + page;
+			document.getElementById("pager").innerHTML = page + "/" + lastPage;
 		} else
 		if (page == 1 || page < 1) {
 			document.getElementById("page-frame").src = "assets/pages/page" + lastPage + ".html";
 			page = lastPage;
-			document.getElementById("pager").innerHTML = "P:" + page;
+			document.getElementById("pager").innerHTML = page + "/" + lastPage;
 		}
 		if (page > lastPage) {
 			document.getElementById("page-frame").src = "assets/pages/page" + lastPage + ".html";
 			page = lastPage;
-			document.getElementById("pager").innerHTML = "P:" + page;
+			document.getElementById("pager").innerHTML = page + "/" + lastPage;
 		}
+	}
+}
+
+function ControlKey(event) {
+	if (event.keyCode == 37) {
+		changePage("left");
+	}
+	else if (event.keyCode == 39) {
+		changePage("right");
 	}
 }
